49aab4d7271428113efb023182ccb05592c90965
ezhil srikanth <mr.srik307@gmail.com>
2025-08-10 19:40:09 +0530

    new test

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { Server } = require("socket.io");
 const http = require("http");
+require("dotenv").config();
 
 const server = http.createServer();
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+  },
+  pingTimeout: 60000,
+  pingInterval: 25000,
+  transports: ['websocket'],
+  allowEIO3: true,
 });
+
+
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
@@ -15,6 +24,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Socket.IO server running on port 3000");
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Socket.IO server running on port " + (process.env.PORT || 3000));
 });
